617380a3932dc4f0fba57a1d26597d5b22424aaa
tanu <dastanushri402@gmail.com>
2024-10-22 00:33:40 +0600

    add phone input field in contact us home of home page

--- src/Pages/Home/ContactUs/ContactUs.jsx
+++ src/Pages/Home/ContactUs/ContactUs.jsx
@@ -1,30 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdLocationOn } from "react-icons/md";
 import { BsFillTelephonePlusFill } from "react-icons/bs";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import Button from "../../../Components/Shared/Button/Button";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
 const ContactUs = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const [axiosSecure] = useAxiosSecure();
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const onSubmit = async (data) => {
+    // Extract countryCode and phone from phoneNumber
+    const countryCode = phoneNumber.slice(0, phoneNumber.length - 10);
+    const phone = phoneNumber.slice(-10);
+
     try {
       const newContact = {
         name: data.name,
         email: data.email,
-        phone: data.phone,
+        phone,
+        countryCode,
         message: data.message,
       };
       axiosSecure.post("/contacts", newContact).then((data) => {
         reset();
+        setPhoneNumber("");
         if (data.data.insertedId) {
           Swal.fire({
             position: "top-end",
@@ -39,9 +43,10 @@ const ContactUs = () => {
       console.error("Error storing contact data:", error);
     }
   };
+
   return (
-    <div className="md:mx-12 xl:mx-20 my-12 grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-6 bg-[#07332f] rounded-xl py-12">
-      <div>
+    <div className="md:mx-12 xl:mx-20 my-12 flex flex-col xl:flex-row justify-center items-center gap-6 bg-[#07332f] rounded-xl p-2 sm:p-6 md:p-10">
+      <div className="w-full xl:w-1/2">
         <h2 className="text-4xl font-bold mb-5 text-center text-white">
           Contact With Us
         </h2>
@@ -58,43 +63,45 @@ const ContactUs = () => {
           <p className="text-[16px] text-white">Dhanmondi, Dhaka, Bangladesh</p>
         </div>
       </div>
-      <div>
+      <div className="w-full xl:w-1/2">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="space-y-4 p-8 bg-white rounded-xl max-w-lg mx-auto"
+          className="space-y-4 p-8 bg-white rounded-xl max-w-xl mx-auto"
         >
-          <div>
-            <input
-              type="text"
-              placeholder="Name"
-              className="form-input text-base w-full"
-              {...register("name", { required: true, maxLength: 90 })}
-            />
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-3 md:gap-6">
-            <div>
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="w-full">
+              <input
+                type="text"
+                placeholder="Name"
+                className="border text-black border-gray-300 rounded-lg w-full p-3 outline-none"
+                {...register("name", { required: true, maxLength: 90 })}
+              />
+            </div>
+            <div className="w-full">
               <input
                 type="email"
                 placeholder="Email Address"
-                className="form-input text-base"
+                className="border text-black border-gray-300 rounded-lg w-full p-3 outline-none"
                 {...register("email", { required: true, maxLength: 90 })}
               />
             </div>
-            <div>
-              <input
-                type="number"
-                placeholder="Phone Number"
-                className="form-input text-base"
-                {...register("phone", { required: true, maxLength: 90 })}
-              />
-            </div>
           </div>
-
+          <div>
+            <PhoneInput
+              country={"bd"}
+              value={phoneNumber}
+              onChange={setPhoneNumber}
+              inputProps={{
+                className:
+                  "border text-black border-gray-300 rounded-lg w-full ps-12 py-3 outline-none",
+              }}
+            />
+          </div>
           <div>
             <textarea
               resize="none"
-              className="form-input h-24 text-base"
-              placeholder="Your Message"
+              placeholder="Your message"
+              className="border h-24 text-black border-gray-300 outline-none rounded-lg w-full p-3"
               {...register("message", { required: true })}
             ></textarea>
           </div>
